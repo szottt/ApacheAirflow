4d39a61a3303c6b0671bf31ef4f87bc9777e4ed3
Igor Szot <szottt@hotmail.com>
2022-02-04 15:43:54 -0300

    Ajuste no shell

--- airflow.sh
+++ airflow.sh
@@ -2,15 +2,25 @@
 
 echo "Agora irei iniciar o AirFLow para voce estudar!"
 
-source ~/Documentos/Cursos/Udemy/ApacheAirflow/venv/bin/activate
+#source /home/vkn/Documentos/Cursos/Udemy/ApacheAirflow/venv/bin/activate
+#./home/vkn/Documentos/Cursos/Udemy/ApacheAirflow/venv/bin/activate
 
 airflow webserver --port 8080 -D
 
 airflow scheduler -D
 
 echo "Agora o airflow esta em pe!"
-echo "Aprovete seus estudos
+echo "Aprovete seus estudos"
 
 echo "Para olhar seu airflow lembre de entrar no localhost/8080"
+
+cd /home/vkn/airflow/dags
+
+
+echo "Caminho alterado para pasta de Dags"
+
+echo "Agora abri o vscode para editar os Dags"
+
+code .
 
 
